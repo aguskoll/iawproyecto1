22948b2c579e65e618c0a87413c83647c49c3d48
sebalr <sebalr@seba-pc>
2016-04-06 16:30:51 -0300

    lista desde get

--- php/db.php
+++ php/db.php
@@ -1,15 +1,16 @@
- <?php  
+<?php  
 class Model {
-        # @object, data base
+    # @object, base de datos
     private $db;
 
-    # @bool ,  Connected to the database
+    # @bool ,  conectado a la bd
     private $dbConnected = false;
-
+    
+    #Creo el objeto bd y la tabla si no existe
     public function __construct() {
         try {
             //Creo conexion a bd y las tablas
-            $this->Connect();
+            $this->connect();
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
             
             $query = "CREATE TABLE IF NOT EXISTS todo (Id INTEGER PRIMARY KEY, nota TEXT, fecha TEXT, orden INTEGER, hecha INTEGER, url TEXT)";
@@ -20,7 +21,8 @@ class Model {
         }
     }
     
-       private function connect(){
+    #Genera la conexion a la bd
+    private function connect(){
         try {
             $this->db = new PDO('sqlite:materiasDB.sqlite');
             $this->dbConnected = true;
@@ -29,13 +31,20 @@ class Model {
         }
     }
     
+    #Agrega una nueva nota
     public function addNota($nota, $fecha, $orden,$hecha, $url){
+        if(!$dbConnected){
+            $this->connect();
+        }
         $query = "INSERT INTO todo (nota, fecha, orden, hecha, url) VALUES('$nota', '$fecha', $orden, $hecha, '$url')";
         $this->db->exec($query);
     }
     
     //Retorna las notas correspondients a la url dada
     public function getNotas($url){
+        if(!$dbConnected){
+            $this->connect();
+        }
         return $this->db->query("SELECT nota, fecha, orden FROM todo WHERE url = '$url' ORDER BY orden ASC")->fetchAll();
     }
 }
--- php/index.php
+++ php/index.php
@@ -1,26 +1,37 @@
 <!DOCTYPE HTML>
 <html>
+
 <body>
 
-Mostrar las notas <br>
-<?php
+    <?php
     include_once('db.php');
     $bd = new Model();
-    $bd->addNota("seba", "manana", 33,0,"l2");
-    $notas = $bd->getNotas("l2");
-    echo "<table border=1>";
-    echo "<tr><td>Nota</td><td>Fecha</td><td>Orden</td></tr>";
- 
-    foreach ($notas as $nota) {
-       echo "<tr>";
-       echo "<td>".$nota['nota']."</td>";
-       echo "<td>".$nota['fecha']."</td>";
-       echo "<td>".$nota['orden']."</td>";
-       echo "</tr>";
-       }
-       echo "</table>";
+    if(!isset($_GET['listaID'])){
+        //generar un identificador de lista con la php session
+        echo "<br> crear una nueva id";
+    }else{
+        $lista=$_GET["listaID"];
+       
+        //$bd->addNota("seba", "manana", 33,0,"l2");
+        $notas = $bd->getNotas($lista);
+        if($notas == false || (count($notas) > 0)){
+            echo "<br> la url es invalida, no existe esa lista";
+        }else{
+        echo "<table border=1>";
+        echo "<tr><td>Nota</td><td>Fecha</td><td>Orden</td></tr>";
 
-?>
-    
+        foreach ($notas as $nota) {
+           echo "<tr>";
+           echo "<td>".$nota['nota']."</td>";
+           echo "<td>".$nota['fecha']."</td>";
+           echo "<td>".$nota['orden']."</td>";
+           echo "</tr>";
+           }
+           echo "</table>";
+        }
+    }
+    ?>
+
 </body>
+
 </html>
